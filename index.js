991d7e20b021c5fd378db61ee4a2cec7045f3741
chrisvsanchez <chrisvsanchez@gmail.com>
2020-08-06 21:23:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    let dataForm = {
+        name: name,
+        email: email
+    };
+
+    let configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(dataForm)
+    };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(response => response.json())
+        .then(data => {
+            document.body.prepend(data.id);
+        })
+        .catch(error => {
+            document.body.append(error.message);
+        })
+    }
